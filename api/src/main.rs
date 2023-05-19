fb8f393738639faa483ec518ac6cfe4fa1458a27
rindy1123 <genryusinbaou@gmail.com>
2023-05-19 14:03:53 -0700

    feat: install & setup Rocket
    What I did
    - Install Rocket & serde
    - Setup cors
    - Create test API(which will be deleted)

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use fairing::cors::Cors;
+use rocket::{
+    figment::providers::{Format, Toml},
+    http::Status,
+    serde::json::Json,
+    Config,
+};
+use serde::Serialize;
+
+#[macro_use]
+extern crate rocket;
+mod fairing;
+
+// TODO: delete after testing
+#[derive(Serialize)]
+pub struct Test {
+    pub success: bool,
+}
+
+// TODO: delete after testing
+#[get("/")]
+pub async fn test() -> Json<Test> {
+    let res = Test { success: true };
+    Json(res)
+}
+
+#[options("/<_..>")]
+fn cors_handler() -> Status {
+    Status::NoContent
+}
+
+#[launch]
+fn rocket() -> _ {
+    let apis = routes![test, cors_handler];
+    let figment = Config::figment().merge(Toml::file("App.toml").nested());
+    rocket::custom(figment).mount("/", apis).attach(Cors)
 }

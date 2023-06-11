c1095eae6790da2fa1d90c718adab3194c46633f
miwa <61976102+miwa223@users.noreply.github.com>
2023-06-11 14:39:40 -0700

    fix an indentation format

--- api/src/controller/rooms.rs
+++ api/src/controller/rooms.rs
@@ -66,8 +66,8 @@ pub async fn index(db: &DB) -> Result<Json<view::room::List>, Status> {
 pub async fn update(
     id: String,
     room: Json<RoomParams>,
-    db: &DB)
-    -> Result<Json<view::room::Get>, Status> {
+    db: &DB,
+) -> Result<Json<view::room::Get>, Status> {
     let RoomParams {
         title,
         price,

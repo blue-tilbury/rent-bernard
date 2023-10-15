aca5e0a43fdaae3e87cb57adb2f1dc7dbb592562
rindy1123 <genryusinbaou@gmail.com>
2023-10-14 21:09:43 -0700

    fix: fix room image factory

--- api/src/model/room_image/factory.rs
+++ api/src/model/room_image/factory.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 pub mod tests {
-    use sqlx::PgPool;
+    use sqlx::{PgPool, Row};
     use uuid::Uuid;
 
     use crate::model::room_image::model::RoomImage;
@@ -15,24 +15,24 @@ pub mod tests {
 
     impl RoomImageFactory {
         pub async fn create(db: &PgPool, params: RoomImageFactoryParams) -> RoomImage {
-            let rec = sqlx::query!(
+            let rec = sqlx::query(
                 r#"
                     INSERT INTO room_images ( room_id, s3_key )
                     VALUES ( $1, $2 )
                     RETURNING *
                 "#,
-                params.room_id,
-                params.s3_key,
             )
+            .bind(params.room_id)
+            .bind(params.s3_key)
             .fetch_one(db)
             .await
             .unwrap();
             RoomImage {
-                id: rec.id,
-                room_id: rec.room_id,
-                s3_key: rec.s3_key,
-                created_at: rec.created_at,
-                updated_at: rec.updated_at,
+                id: rec.try_get("id").unwrap(),
+                room_id: rec.try_get("room_id").unwrap(),
+                s3_key: rec.try_get("s3_key").unwrap(),
+                created_at: rec.try_get("created_at").unwrap(),
+                updated_at: rec.try_get("updated_at").unwrap(),
             }
         }
 

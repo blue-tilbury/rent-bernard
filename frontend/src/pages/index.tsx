5570ff7a63d94f9c0015e4e956f27203628ad13d
miwa <61976102+miwa223@users.noreply.github.com>
2023-10-17 19:17:13 -0700

    add your reviews page

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { ErrorPage } from "./ErrorPage";
+import { Login } from "./Login";
+import { Posting } from "./Posting";
+import { Search } from "./Search";
+import { ThankYou } from "./ThankYou";
+import { Wishlist } from "./Wishlist";
+import { YourAds } from "./YourAds";
+import { YourReviews } from "./YourReviews";
+
+export { ErrorPage, Login, Posting, Search, ThankYou, Wishlist, YourAds, YourReviews };

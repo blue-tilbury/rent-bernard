e4efacd3d8874770dd8dc639cb01c8fb6d6c51ff
miwa <61976102+miwa223@users.noreply.github.com>
2023-10-30 12:20:20 -0700

    fix rgb to hex

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -21,7 +21,7 @@ module.exports = {
         "rent-dark-green": "#1dcc00",
         "rent-bg-gray": "#F5F7F7",
         "rent-input-gray": "#d7d7dc",
-        "rent-very-light-gray": "rgb(229, 231, 235)",
+        "rent-very-light-gray": "#e5e7eb",
         "rent-light-gray": "#adafb7",
         "rent-gray": "#6f727f",
       },

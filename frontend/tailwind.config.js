d849a68c0e4625a7312315e25f02dde9dc82e81a
miwa <61976102+miwa223@users.noreply.github.com>
2023-11-01 18:43:18 -0700

    fix flex grow

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       flex: {
         2: "2 2 0%",
         3: "3 3 0%",
-        "1/3": "1 1 33.333333%",
+        "1/3": "0 1 33.333333%",
       },
       colors: {
         "rent-very-light-blue": "#4edcff",

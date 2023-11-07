45dfcba8956b9fada8f4881c075f584cf29c4ebb
miwa <61976102+miwa223@users.noreply.github.com>
2023-11-07 11:57:50 -0800

    modify thankyou page

--- frontend/src/pages/ThankYou.tsx
+++ frontend/src/pages/ThankYou.tsx
@@ -6,8 +6,10 @@ import { Button } from "../components/Button";
 export const ThankYou = () => {
   return (
     <section className="container flex flex-col items-center pb-24 pt-16">
-      <CheckCircleIcon className="h-60 w-60 text-rent-dark-green" />
-      <h2 className="pb-3 pt-6 text-3xl font-bold">Thank you!</h2>
+      <CheckCircleIcon className="h-36 w-36 text-rent-dark-green lg:h-60 lg:w-60" />
+      <h2 className="pb-3 pt-6 text-xl font-semibold lg:text-3xl lg:font-bold">
+        Thank you!
+      </h2>
       <p className="pb-16">Your submission has been sent.</p>
       <Link to="/">
         <Button size="lg" color="primary" type="button">
